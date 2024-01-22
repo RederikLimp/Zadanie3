74d1c7e01e75f98803a20e300175199b1a77e9e8
RederikLimp <kirill.alisa.2004@mail.ru>
2024-01-22 12:05:02 +0700

    Удаление столбцов

--- Zadanie 3/js/main.js
+++ Zadanie 3/js/main.js
@@ -17,7 +17,7 @@ Vue.component("board", {
           <input type="text" placeholder="Описание" v-model="desc" maxlength="60">
           </div>
           <div>
-          <input type="date" id="start" name="start" v-model="deadline"
+          <input type="date" id="start" name="start" v-model="deadlin"
           min="2023-01-01" max="2030-12-31">
           </div>
           
@@ -27,6 +27,8 @@ Vue.component("board", {
       </form>  
     <ul>
         <li v-for="card in column1">
+        <card @deletethis="Delete" :column=1 :id="card.id" :title="card.title" :desc="card.desc" :deadline="card.deadline" :createtime="card.createtime">
+        </card>
         </li>
         </ul>
     </li>
@@ -34,7 +36,8 @@ Vue.component("board", {
         <h3>Задачи в работе</h3>
         <ul>
             <li v-for="card in column2">
-                <card :column=2></card>
+            <card @deletethis="Delete" :column=2>
+            </card>
             </li>
         </ul>
     </li>
@@ -42,7 +45,8 @@ Vue.component("board", {
         <h3>Тестирование</h3> 
         <ul>
             <li v-for="card in column3">
-                <card :column=3></card>
+            <card @deletethis="Delete" :column=3>
+            </card>
             </li>
         </ul> 
     </li>
@@ -50,89 +54,136 @@ Vue.component("board", {
         <h3>Выполненные задачи</h3>  
         <ul>
             <li v-for="card in column4">
-                <card :column=4></card>
+            <card @deletethis="Delete" :column=4>
+            </card>
             </li>
         </ul>
     </li>
     </ul>
     </div>
         `,
-        data() {
-            return{
-                column1:[],
-                column2:[],
-                column3:[],
-                column4:[],
-    
-                allcolumns:[],
-    
-                title:null,
-                desc:null,
-                deadline:null,
-    
+    data() {
+        return {
+            column1: [],
+            column2: [],
+            column3: [],
+            column4: [],
+            id: 0,
+            allcolumns: [],
+
+            title: null,
+            desc: null,
+            year: null,
+            month: null,
+            day: null,
+            deadlin: null,
+            deadline: [],
+
+        }
+    },
+    methods: {
+        CreateCard() {
+            if (this.title && this.desc && this.deadlin) {
+                this.year = this.deadlin[0] + this.deadlin[1] + this.deadlin[2] + this.deadlin[3]
+                this.month = this.deadlin[5] + this.deadlin[6]
+                this.day = this.deadlin[8] + this.deadlin[9]
+
+                this.deadline.push({ day: this.day, month: this.month, year: this.year })
+                let createtime = new Date()
+                info = {
+                    id: this.id,
+                    title: this.title,
+                    desc: this.desc,
+                    deadline: this.deadline,
+                    createtime: String(createtime),
+                    edit: false,
+                }
+                this.id += 1
+                this.column1.push(info)
             }
         },
-        methods: {
-            CreateCard(){
-                let createtime = new Date()
-                info ={
-                    title:this.title,
-                    desc:this.desc,
-                    deadline:this.deadline,
-                    createtime:createtime,
+        Delete(id) {
+            for (let i = 0; i < this.column1.length; i++) {
+                if (this.column1[i].id == id) {
+                    this.column1.splice(i, 1)
                 }
-                this.column1.push(info)
-            },
-        },
-        mounted() {
-        },
-        props:{  
-        },
-        computed: {
+            }
+            for (let i = 0; i < this.column2.length; i++) {
+                if (this.column2[i].id == id) {
+                    this.column2.splice(i, 1)
+                }
+            }
+            for (let i = 0; i < this.column3.length; i++) {
+                if (this.column3[i].id == id) {
+                    this.column3.splice(i, 1)
+                }
+            }
+            for (let i = 0; i < this.column4.length; i++) {
+                if (this.column4[i].id == id) {
+                    this.column4.splice(i, 1)
+                }
+            }
         }
-    });
-    Vue.component("card", {
-        template: `
+    },
+    mounted() {
+    },
+    props: {
+    },
+    computed: {
+    }
+});
+Vue.component("card", {
+    template: `
     <div class="card">
+    <p>{{this.id}}</p>
     <p>{{this.title}}</p>
     <p>{{this.desc}}</p>
     <p>{{this.deadline}}</p>
     <p>{{this.createtime}}</p>
+    <div>
+    <button v-on:click="deletethis">Удалить карточку</button>
+    </div>
     </div>
         `,
-        data() {
-            return{
-            }
-        },
-        methods: {
-        },
-        mounted() {
-        },
-        props:{ 
-            column:{
-                type:Number
-            },
-            title:{
-                type:String
-            },
-            desc:{
-                type:String
-            },
-            deadline:{
-                type:String
-            },
-            createtime:{
-                type:Date
-            }
-    
-        },
-        computed: {
+    data() {
+        return {
         }
-    });
-    let app = new Vue({
-        el: "#app",
-        data: {
+    },
+    methods: {
+        deletethis(){
+            this.$emit("deletethis",this.id);
+        }
+    },
+    mounted() {
+    },
+    props: {
+        column: {
+            type: Number
         },
-        methods: {
+        id:{
+            type:Number
         },
-    });
+        title: {
+            type: String
+        },
+        desc: {
+            type: String
+        },
+        deadline: {
+            type:Array
+        },
+        createtime: {
+            type:String
+        }
+
+    },
+    computed: {
+    }
+});
+let app = new Vue({
+    el: "#app",
+    data: {
+    },
+    methods: {
+    },
+});
