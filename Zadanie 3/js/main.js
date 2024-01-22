ea1259e8f35d701b1e5828d921eb7700bf35493c
RederikLimp <kirill.alisa.2004@mail.ru>
2024-01-22 11:15:51 +0700

    Разбивка столбцов

--- Zadanie 3/js/main.js
+++ Zadanie 3/js/main.js
@@ -1,12 +1,52 @@
 let eventBus = new Vue()
-
 Vue.component("board", {
     template: `
 <div class="board">
+<ul class="columns">
+<li class="column">
+    <h3>Запланированные задачи</h3>  
+    <ul>
+        <li v-for="card in column1">
+            <card :column=1></card>
+        </li>
+    </ul>
+</li>
+<li class="column">
+    <h3>Задачи в работе</h3>
+    <ul>
+        <li v-for="card in column2">
+            <card :column=2></card>
+        </li>
+    </ul>
+</li>
+<li class="column">
+    <h3>Тестирование</h3> 
+    <ul>
+        <li v-for="card in column3">
+            <card :column=3></card>
+        </li>
+    </ul> 
+</li>
+<li class="column">
+    <h3>Выполненные задачи</h3>  
+    <ul>
+        <li v-for="card in column4">
+            <card :column=4></card>
+        </li>
+    </ul>
+</li>
+</ul>
 </div>
     `,
     data() {
         return{
+            column1:[],
+            column2:[],
+            column3:[],
+            column4:[],
+
+            allcolumns:[],
+
         }
     },
     methods: {
@@ -19,12 +59,33 @@ Vue.component("board", {
     }
 });
 
+Vue.component("card", {
+    template: `
+<div class="card">
+</div>
+    `,
+    data() {
+        return{
+        }
+    },
+    methods: {
+    },
+    mounted() {
+    },
+    props:{ 
+        column:{
+            type:Boolean
+        } 
+    },
+    computed: {
+    }
+});
+
 
 let app = new Vue({
     el: "#app",
     data: {
     },
     methods: {
-
     },
 });
